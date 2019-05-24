d6447bb7920d9bc07a615e90ac133ebb65a46b18
Bakari Mustafa <techmustafa31@gmail.com>
2019-05-24 18:51:41 +1000

    Update test_program.py

--- DNS & IPA/test_program.py
+++ DNS & IPA/test_program.py
@@ -1,9 +1,9 @@
-from Blacklist import*
+from Blacklist import *
 
 blacklist_domain = Blacklist()
 
 while True:
-    CommandDNS = input("👉 ")
+    CommandDNS = input(">>> ")
     Command = CommandDNS.split()
     if Command[0] == "blacklist":
         blacklist_domain.newblacklist(Command[1])
@@ -11,26 +11,23 @@ while True:
         if "find" in Command:
             print(blacklist_domain.domain_lookup(Command[1]))
     elif len(Command) == 3:
-        if "change" in Command:
-            print(blacklist_domain.uptodatedomain(Command[1],Command[2]))
+        if "update" in Command:
+            print(blacklist_domain.update(Command[1], Command[2]))
         elif "add" in Command:
-            print(blacklist_domain.change(Command[1],Command[2]))
+            print(blacklist_domain.change(Command[1], Command[2]))
 
     else:
         print("Error 👻")
 
 """
-Scripting
+Scripting >>>
 
-Python Terminal
-    1. Un-comment these lines
-    2. Run find find domain_lookup bakarimustafa.com
-    3. Find domain_lookup
-    4. change domain_lookup / enter domain of choice
-    
-Common Scriptting lines
-    Find / domain
-    Change / domain
-    Add / domain
-    Blacklist / domain
+Program Input example:
+    add domain_lookup bakarimustafa.com
+    update domain_lookup google.com
+    find domain_lookup
+    add domain_lookup google.com
+    find domain_lookup
+    blacklist newblacklist griffith.edu.au
+    update domain_lookup fb.com
 """
